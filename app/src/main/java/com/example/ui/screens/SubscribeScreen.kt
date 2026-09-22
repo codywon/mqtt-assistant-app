d08ecbd749fae022ede9bd49694de95b5f5961c9
codywon <290923628@qq.com>
2026-09-22 16:48:19 +0800

    fix: import BorderStroke in SubscribeScreen

--- app/src/main/java/com/example/ui/screens/SubscribeScreen.kt
+++ app/src/main/java/com/example/ui/screens/SubscribeScreen.kt
@@ -3,6 +3,7 @@ package com.example.ui.screens
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
